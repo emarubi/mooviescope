fe943c32080c600f3bd59cd8fb25e1af4dfc501d
emarubi <69473685+emarubi@users.noreply.github.com>
2022-05-07 16:03:39 +0200

    Added photo credits

--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -4,8 +4,26 @@ import MovieList from './components/MoviesList';
 import MovieDetails from './components/MovieDetails';
 import HomePage from './components/HomePage';
 import { BrowserRouter } from "react-router-dom";
+import { useEffect } from 'react';
 
 function App() {
+  const addCredits = () => {
+    const credits = document.createElement('a')
+    credits.setAttribute('href', 'https://unsplash.com/photos/G16kFHYvCoQ')
+    credits.setAttribute('style',  "position: absolute; bottom: 10px; left: 10px; color: white; font-size: 11px")
+    credits.textContent = "Photo by Paolo Chiabrando"
+    document.body.appendChild(credits)
+  }
+  
+  useEffect(() => {
+    addCredits()
+    return function cleanup () {
+     const element= document.getElementById("credits")
+     if (element) {
+       document.removeChild(element)
+     }
+    }
+  })
 
   return (
     <>
